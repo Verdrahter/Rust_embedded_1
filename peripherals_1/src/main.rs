022f47ff70491b12fe09c8ce39add6f336133504
Ver Drahter <verdrahter@gmail.com>
2022-06-01 22:31:21 +0200

    Funktionen für einzelne Registertypen

--- peripherals_1/src/main.rs
+++ peripherals_1/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 // pick a panicking behavior
+use crate::tm4c129x::gpio_porta_ahb::_PC;
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
                      // use panic_abort as _; // requires nightly
                      // use panic_itm as _; // logs messages over ITM; requires ITM support
@@ -9,10 +10,59 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 //use cortex_m::asm;
 use cortex_m_rt::entry;
-use tm4c129x::{self, generic::Reg, sysctl::_RCGCGPIO};
+use tm4c129x::{
+    self,
+    generic::{Readable, Reg},
+    gpio_porta_ahb::{_DEN, _DIR, _PUR},
+    sysctl::_RCGCGPIO,
+};
 
-fn set_bits(reg: &Reg<u32, _RCGCGPIO>, bit: &[u8]) {
-    let mut bits = reg.read().bits();
+fn read_bits<T>(reg: &Reg<u32, T>) -> u32
+where
+    Reg<u32, T>: Readable,
+{
+    let bits = reg.read().bits();
+    return bits;
+}
+
+fn set_bits_rcgcgpio(reg: &Reg<u32, _RCGCGPIO>, bit: &[u8]) {
+    let mut bits = read_bits(reg);
+    for b in bit {
+        bits |= 1 << b;
+    }
+    reg.write(|w| unsafe { w.bits(bits) });
+}
+
+fn set_bits_dir(reg: &Reg<u32, _DIR>, bit: &[u8]) {
+    let mut bits = read_bits(reg);
+    for b in bit {
+        if b < &0x80 {
+            bits |= 1 << b;
+        } else {
+            bits &= !(1 << !b);
+        }
+    }
+    reg.write(|w| unsafe { w.bits(bits) });
+}
+
+fn set_bits_den(reg: &Reg<u32, _DEN>, bit: &[u8]) {
+    let mut bits = read_bits(reg);
+    for b in bit {
+        bits |= 1 << b;
+    }
+    reg.write(|w| unsafe { w.bits(bits) });
+}
+
+fn set_bits_pur(reg: &Reg<u32, _PUR>, bit: &[u8]) {
+    let mut bits = read_bits(reg);
+    for b in bit {
+        bits |= 1 << b;
+    }
+    reg.write(|w| unsafe { w.bits(bits) });
+}
+
+fn set_bits_pc(reg: &Reg<u32, _PC>, bit: &[u8]) {
+    let mut bits = read_bits(reg);
     for b in bit {
         bits |= 1 << b;
     }
@@ -25,45 +75,31 @@ fn main() -> ! {
 
     // Enable clock for ports F, J,N
     let bits_to_set = [5, 8, 12];
-    set_bits(&p.SYSCTL.rcgcgpio, &bits_to_set);
+    set_bits_rcgcgpio(&p.SYSCTL.rcgcgpio, &bits_to_set);
 
     // D1 and D2 are connected to GPIOs PN1 and PN0. These
     // LEDs are dedicated for use by the software application
-    let mut bits = p.GPIO_PORTN.dir.read().bits();
-    bits |= 0x03;
-    p.GPIO_PORTN.dir.write(|w| unsafe { w.bits(bits) });
-
-    bits = p.GPIO_PORTN.den.read().bits();
-    bits |= 0x03;
-    p.GPIO_PORTN.den.write(|w| unsafe { w.bits(bits) });
+    let bits_to_set: [u8; 2] = [0, 1];
+    set_bits_dir(&p.GPIO_PORTN.dir, &bits_to_set);
+    set_bits_den(&p.GPIO_PORTN.den, &bits_to_set);
 
     // D3 and D4 are connected to GPIOs PF4 and PF0, which can be controlled by user’s
     // software or the integrated Ethernet module of the microcontroller.
-    bits = p.GPIO_PORTF_AHB.dir.read().bits();
-    bits |= 0x11;
-    p.GPIO_PORTF_AHB.dir.write(|w| unsafe { w.bits(bits) });
-
-    bits = p.GPIO_PORTF_AHB.den.read().bits();
-    bits |= 0x11;
-    p.GPIO_PORTF_AHB.den.write(|w| unsafe { w.bits(bits) });
+    let bits_to_set: [u8; 2] = [0, 4];
+    set_bits_dir(&p.GPIO_PORTF_AHB.dir, &bits_to_set);
+    set_bits_den(&p.GPIO_PORTF_AHB.den, &bits_to_set);
 
     // Two user switches are provided for input and control of the TM4C1294NCPDTI software. The switches
     // are connected to GPIO pins PJ0 and PJ1.
-    bits = p.GPIO_PORTJ_AHB.dir.read().bits();
-    bits &= !0x03;
-    p.GPIO_PORTJ_AHB.dir.write(|w| unsafe { w.bits(bits) });
+    let bits_to_set: [u8; 2] = [!0, !1];
+    set_bits_dir(&p.GPIO_PORTJ_AHB.dir, &bits_to_set);
 
-    bits = p.GPIO_PORTJ_AHB.den.read().bits();
-    bits |= 0x03;
-    p.GPIO_PORTJ_AHB.den.write(|w| unsafe { w.bits(bits) });
-
+    let bits_to_set: [u8; 2] = [0, 1];
+    set_bits_den(&p.GPIO_PORTJ_AHB.den, &bits_to_set);
     // pull-up resistor
-    bits = p.GPIO_PORTJ_AHB.pur.read().bits();
-    bits |= 0x03;
-    p.GPIO_PORTJ_AHB.pur.write(|w| unsafe { w.bits(bits) });
-
+    set_bits_pur(&p.GPIO_PORTJ_AHB.pur, &bits_to_set);
     // ???
-    p.GPIO_PORTJ_AHB.pc.write(|w| unsafe { w.bits(3) });
+    set_bits_pc(&p.GPIO_PORTJ_AHB.pc, &bits_to_set);
 
     let mut lastsw1 = false;
     let mut lastsw2 = false;
